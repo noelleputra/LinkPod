345e1c3e05da813d0cf829322e97b40b91ccc720
noelleputra <noellenputra@gmail.com>
2026-07-07 16:29:45 +0700

    Config Debug

--- src/config/config.h
+++ src/config/config.h
@@ -8,9 +8,9 @@ namespace config {
     constexpr uint32_t UART_BAUD = 9600;
     constexpr uint32_t REQUEST_INTERVAL_MS = 40;
     constexpr size_t LINE_BUFFER_SIZE = 64;
-    constexpr uint32_t RS485_TURNAROUND_US = 1000;
+    constexpr uint32_t RS485_TURNAROUND_US = 2000;
     constexpr uint8_t POLL_NODE_IDS[] = {1, 2, 3, 4};
     constexpr size_t POLL_NODE_COUNT = sizeof(POLL_NODE_IDS) / sizeof(POLL_NODE_IDS[0]);
     constexpr uint8_t MAX_RETRIES = 3;
-    constexpr uint32_t RESPONSE_TIMEOUT_MS = 300;
+    constexpr uint32_t RESPONSE_TIMEOUT_MS = 200;
 }
